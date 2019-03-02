e0867ebb67935c0286e02708854e03c5dd6e3ae4
Luciano Palma <lucianopalma90@gmail.com>
2019-03-02 12:13:32 +0000

    Refactor out code

--- III.tdd-katas/password-verifier/password-verifier.js
+++ III.tdd-katas/password-verifier/password-verifier.js
@@ -1,9 +1,9 @@
 const verifier = (password) => {
     validate(() => password == null, "Password must not be null")
     validate(() => password.length < 9, "Password must be larger than 8 characters")
-    validate(() => password.split().every((char) => char.toLowerCase() == char), "Password must contain at least 1 uppercase character")
-    validate(() => password.split().every((char) => char.toUpperCase() == char), "Password must contain at least 1 lowercase character")
-    validate(() => password.split().every((char) => typeof(char) !== "number"), "Password must contain at least 1 number")
+    validate(() => password.toLowerCase() == password, "Password must contain at least 1 uppercase character")
+    validate(() => password.toUpperCase() == password, "Password must contain at least 1 lowercase character")
+    validate(() => password.split("").every((char) => isNaN(char)), "Password must contain at least 1 number")
 }
 
 const validate = (failsValidation, errorMsg) => {
