f7f112e352e3cb80ceeba0df79408faac61be636
Luciano Palma <lucianopalma90@gmail.com>
2019-03-10 17:54:25 +0000

    Extract validation functions

--- III.tdd-katas/password-verifier/password-verifier.js
+++ III.tdd-katas/password-verifier/password-verifier.js
@@ -1,24 +1,40 @@
 class PasswordVerifier {
     verify(password) {
-        if (password == null) {
-            throw Error("password should not be null")
-        }
+        notNull(password)
+        largerThanEightCharacters(password)
+        atLeastOneUpperCaseCharacter(password)
+        atLeastOneLowerCaseCharacter(password)
+        atLeastOneNumber(password)
+    }
+}
 
-        if (password.length < 9) {
-            throw Error("password should be larger than 8 chararacters")
-        }
+const notNull = (password) => {
+    if (password == null) {
+        throw Error("password should not be null")
+    }
+}
 
-        if (password.toLowerCase() == password) {
-            throw Error("password should have one uppercase letter at least")
-        }
+const largerThanEightCharacters = (password) => {
+    if (password.length < 9) {
+        throw Error("password should be larger than 8 chararacters")
+    }
+}
 
-        if (password.toUpperCase() == password) {
-            throw Error("password should have one lowercase letter at least")
-        }
+const atLeastOneUpperCaseCharacter = (password) => {
+    if (password.toLowerCase() == password) {
+        throw Error("password should have one uppercase letter at least")
+    }
+}
 
-        if(password.split("").every(char => isNaN(char))) {
-            throw Error("password should have one number at least")
-        }
+const atLeastOneLowerCaseCharacter = (password) => {
+    if (password.toUpperCase() == password) {
+        throw Error("password should have one lowercase letter at least")
+    }
+}
+
+const atLeastOneNumber = (password) => {
+    if (password.split("").every(char => isNaN(char))) {
+        throw Error("password should have one number at least")
     }
 }
 
