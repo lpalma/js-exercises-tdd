568a6dab804e63badb78f66e93d4c1a01bc1215d
Luciano Palma <lucianopalma90@gmail.com>
2019-03-10 16:54:27 +0000

    Password should be larger than 8 characters

--- III.tdd-katas/password-verifier/password-verifier.test.js
+++ III.tdd-katas/password-verifier/password-verifier.test.js
@@ -1,10 +1,19 @@
 class PasswordVerifier {
     verify(password) {
-        throw Error("password should not be null")
+        if (password == null) {
+            throw Error("password should not be null")
+        }
+
+        throw Error("password should be larger than 8 chararacters")
     }
 }
 
+const passwordVerifier = new PasswordVerifier()
+
 test("Password should not be null", () => {
-    const passwordVerifier = new PasswordVerifier()
     expect(() => passwordVerifier.verify(null)).toThrow(Error("password should not be null"))
+})
+
+test("Password should be larger than 8 characters", () =>{
+    expect(() => passwordVerifier.verify("abcdefgh")).toThrow(Error("password should be larger than 8 chararacters"))
 })
